c247b52a0bdd694a3cd4087ccf10d99a086cd5d8
Styles-Theandriod <emmanuelajah188@gmail.com>
2024-02-02 15:11:47 +0100

    react native projects maade in class

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, in } from 'react-native';
 import Splashscreen from './components/Splashscreen';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Splashscreen/>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    //   <Splashscreen/>
+    //   <StatusBar style="auto" />
+    // </View>
+
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Splashscreen" component={Splashscreen} options={{title:'Welcome'}}/>
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
