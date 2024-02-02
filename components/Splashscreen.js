f27c2da1814076236117908228c6d118c6909e19
Styles-Theandriod <emmanuelajah188@gmail.com>
2024-02-02 15:12:09 +0100

    made in class

--- components/Splashscreen.js
+++ components/Splashscreen.js
@@ -1,14 +1,46 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image, Button, TouchableOpacity, Pressable } from 'react-native'
 import React from 'react'
-
+const ourImage = require('./Group3.png')
 const Splashscreen = () => {
   return (
-    <View>
-      <Text>Splashscreen</Text>
+    <View style={styles.buttonContainer}>
+      <Image source={ourImage}  style={styles.image}/>
+      <TouchableOpacity style={styles.Button}>
+        <Text>Lets Go</Text>
+      </TouchableOpacity>
+
+      
     </View>
   )
 }
 
 export default Splashscreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'green',
+    },
+    image:{
+        height: '100%',
+        width: 100,
+    },
+    Button:{
+        borderRadius:10,
+        width: '100%',
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexDirection: 'row',
+    },
+    buttonContainer:{
+        width: 320,
+        height:68,
+        padding:3,
+        justifyContent:'center',
+        alignItems: 'center',
+        marginHorizontal:20
+    }
+})
